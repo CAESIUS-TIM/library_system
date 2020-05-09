bf75fc0e41b7d7b36cf4c180a2719f2d322d7041
CAESIUS-TIM <60285058+CAESIUS-TIM@users.noreply.github.com>
2020-05-09 16:50:22 +0800

    filters are updated, excel style is startrd.

--- main.py
+++ main.py
@@ -1,72 +1,143 @@
-"""
-要不要NaN
-"""
 import pandas as pd
+import numpy as np
 import re
 
-src = "asset/博库3.4-4.2.xls"
-dst = "sample1.xls"
-data = pd.read_excel(src)
+# -- init --
+# - file -
+# method 1:
+src = 'asset/博库3.4-4.2.xls'
+dst = 'sample1.xls'
+df = pd.read_excel(src)
+# method 2:
+...
+# - const -
+MIN_SIZE, MAX_SIZE = 17, 31  # not null
+LANG_KEYWORDS = ['chi']  # not null
+MIN_PAGE = 50  # not null
+READER_KEYWORDS = ['幼儿', '中小学', '高职', '高专']  # null
+PRESS = ...  # not null
+TITLE_KEYWORDS = ['报告', '皮书', '年鉴', '研究', '分析']  # not null
+TITLES = ['书名', 'chinesetitle']
+columns = list(df)
+for TITLE in TITLES:
+    if TITLE in columns:
+        break
 
 
-def size_filter(item, inf=17, sup=31):  # 2
+# -- filter_function --
+# print 开头部分：\033[显示方式;前景色;背景色m + 结尾部分：\033[0m
+def print_color(*args, color=34, sep=' ', end='\n',):
+    colors = {
+        'black': 30,
+        'red': 31,
+        'green': 32,
+        'yellow': 33,
+        'blue': 34,
+        'magenta': 35,
+        'cyan': 36,
+        'white': 37,
+    }
+    if isinstance(color, str):
+        color = colors.get(color.lower(), 30)
+    print('\033[%dm' % color+sep.join(map(str, args))+'\033[0m')
+
+
+def size_filter(item):  # 2.去除小于17cm或大于31cm开本的图书。
+    """
+    12
+    12×12
+    12*12
+    """
     try:
-        matched = re.match(r"(\d+)(?:.+?(\d+))?", item)
+        matched = re.match(r'(\d+)(?:.+?(\d+))?', item)
         if matched == None:
             return False
-        if inf <= int(matched.group(1)) <= sup and\
-                (matched.group(2) == None or inf <= int(matched.group(2)) <= sup):
+        if MIN_SIZE <= int(matched.group(1)) <= MAX_SIZE and\
+                (matched.group(2) == None or MIN_SIZE <= int(matched.group(2)) <= MAX_SIZE):
             return True
         else:
             return False
     except:
-        print("[size_filter]error:",item)
+        print_color('[size_filter]error:', item, color='red')
         return False
 
 
-def lang_filter(item, lang=['chi']):  # 3
-    # str or list
+def lang_filter(item):  # 3.去除影印版，影印本，非中文图书。
     try:
-        if item in lang:
+        if item in LANG_KEYWORDS:
             return True
         else:
             return False
     except:
-        print("[lang_filter]error:",item)
+        print_color('[lang_filter]error:', item, color='red')
         return False
 
 
-def reader_filter(item, reader=["幼儿", "中小学", "高职", "高专"]):  # 5
+def reader_filter(item):  # 5.去除读者对象含有幼儿，中小学，甚至高职高专的。
     try:
-        for i in reader:
+        for i in READER_KEYWORDS:
             if i in item:
                 return False
         else:
             return True
     except:
-        print("[reader_filter]error:",item)
+        if pd.isna(item):  # pd.isna > np.isnan
+            return True
+        print_color('[reader_filter]error:', item, color='red')
         return False
 
 
-def page_filter(item, inf=50):  # 9
+def page_filter(item):  # 9.去除小于50页的图书。
+
     try:
-        matched = re.match(r"(\d+)(页)?", item)
-        if inf <= int(matched.group(1)):
+        matched = re.match(r'(\d+)', item)
+        if MIN_PAGE <= int(matched.group(1)):
             return True
         else:
             return False
     except:
-        print("[page_filter]error:",item)
+        print_color('[page_filter]error:', item, color='red')
         return False
 
 
-data.sort_values(by=["分类", "ISBN"], axis=0, ascending=[True, True])  # 12
-data = data.loc[data["尺寸"].apply(size_filter)]
-data = data.loc[data["语种"].apply(lang_filter)]
-data = data.loc[data["读者群"].apply(reader_filter)]
-data = data.loc[data["页数"].apply(page_filter)]
+def title_filter(item):
+    for keyword in TITLE_KEYWORDS:
+        if keyword in value:
+            return True
+    return False
 
-data.to_excel(dst, index=False, encoding="gbk")
+# -- main --
 
-# print(data["定价"][3],type(data["定价"][3])) # 69.0 <class 'numpy.float64'>
-# print(data["卷册"][3],type(data["卷册"][3])) # 2018 <class 'str'>
+
+# 12.按分类号排序，A在最前面。
+df.sort_values(by=['分类', 'ISBN'], axis=0, ascending=[True, True])  # 12
+df = df.loc[df['尺寸'].apply(size_filter)]
+df = df.loc[df['语种'].apply(lang_filter)]
+df = df.loc[df['读者群'].apply(reader_filter)]
+df = df.loc[df['页数'].apply(page_filter)]
+print(df.columns)
+
+# -- excel样式 --
+# method 2:
+# DataFrame.style：便于数据处理
+df.style.apply('color:Magenta', subset=[TITLE])
+
+# -- write --
+# 11.excel进去，出来还是个excel.
+df.to_excel(dst, index=False, encoding='gbk')
+
+"""
+# method 1:
+# excel样式：艺术性高，数据处理性低
+# Warning: 样式必须在最后做！----------------------------
+l_end = len(df.index) + 2  # 表格的行数,便于下面设置格式
+yellow = workbook.add_format({'fg_color': '#FFEE99'})
+
+# 7.高亮显示书名中含有“报告”“皮书”“年鉴”“研究”“分析”的图书信息。
+TITLE_KEYWORDS = ['报告', '皮书', '年鉴', '研究', '分析']
+TITLES = ['书名', 'chinesetitle']
+columns = list(df)
+col_num = columns.find(title)
+for index, value in df[title].items():
+    if(title_filter(value)):
+"""
